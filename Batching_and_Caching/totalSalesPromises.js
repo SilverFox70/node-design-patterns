136054dcca5a51341a3fd1b6414f40373a78d283
Bill Brinkert <william.brinkert@akqa.com>
2017-12-20 13:25:20 -0800

    feat: add beginning of bathcing and caching example folder

---
+++ Batching_and_Caching/totalSalesPromises.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const pify = require('pify');
+const totalSales = pify(require('./totalSales'));
+
+const cache = {};
+
+module.exports = function totalSalesPromises(item) {
+  if (cache[item]) {
+    return cache[item];
+  }
+
+  cache[item] = totalSales(item)
+    .then(res => {
+      setTimeout( () => {delete cache[item]}, 30 * 1000); // 30 seconds expiry
+      return res;
+    })
+    .catch(err => {
+      delete cache[item];
+      throw err;
+    });
+  return cache[item];
+};
